201096679d80cef042dc6eac44a1aee436163887
jacadzaca <vitouejj@gmail.com>
2019-12-28 16:09:25 +0100

    added a function for finding the closing bracket in bf code

--- src/clojure_brainfuck/parser.clj
+++ src/clojure_brainfuck/parser.clj
@@ -13,3 +13,19 @@
               (dec matching-bracket-count)
               matching-bracket-count)))))
     string))
+
+(defn find-coresponding-bracket
+  "Returns index of the corespoding closing bracket or nil if no bracket can be found.
+  Function expects the passed string to have an opening bracket on index n"
+  [string n]
+  (cond 
+    (or (empty? string) (not= (nth string n) \[)) nil
+    :else (loop [i (+ 1 n) matching-bracket-count 1] 
+        (cond 
+          (zero? matching-bracket-count) (- i 1)
+          (= (.length string) i) nil
+          :else (recur (inc i)
+            (case (.charAt string i)
+              \[ (inc matching-bracket-count)
+              \] (dec matching-bracket-count)
+              matching-bracket-count))))))
