0ee7270d90a618e803b4a139106a56a3ac4ea71c
jacadzaca <vitouejj@gmail.com>
2019-12-28 16:05:07 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject clojure-brainfuck "0.1.0-SNAPSHOT"
+  :description "A brainfuck compiler in Clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :main ^:skip-aot clojure-brainfuck.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}}
+  :plugins [[jonase/eastwood "0.3.5"] [lein-kibit "0.1.8"]])
---
+++ src/clojure_brainfuck/core.clj
@@ -0,0 +1,8 @@
+(ns clojure-brainfuck.core
+  (:require [clojure-brainfuck.parser :as parser])
+  (:gen-class))
+
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args])
