079ee58cd991bbcfe1824ce8fb25fb924ac59268
jacadzaca <vitouejj@gmail.com>
2020-01-12 23:10:30 +0100

    added test cases for the optimizing function

---
+++ test/clojure_brainfuck/optimizer_test.clj
@@ -0,0 +1,83 @@
+(ns clojure-brainfuck.optimizer-test
+  (:require [clojure.test :refer [deftest is]]
+            [clojure-brainfuck.optimizer :as optimizer]))
+
+(deftest combines-inc-into-addition-test
+  (is (= [{:type :add :argument 2}] (optimizer/optimize-ast-node [{:type :inc}
+                                                                  {:type :inc}]))))
+
+(deftest combines-inc-into-addition-test
+  (is (= [{:type :sub :argument 2}] (optimizer/optimize-ast-node [{:type :dec}
+                                                                  {:type :dec}]))))
+
+(deftest combines-inc-pointer-into-pointer-addition-test
+  (is (= [{:type :add-pointer :argument 2}] (optimizer/optimize-ast-node [{:type :inc-pointer}
+                                                                           {:type :inc-pointer}]))))
+
+(deftest combines-dec-pointer-into-pointer-substracion-test
+  (is (= [{:type :sub-pointer :argument 2}] (optimizer/optimize-ast-node [{:type :dec-pointer}
+                                                                          {:type :dec-pointer}]))))
+
+(deftest combines-addition-with-inc-test
+  (is (= [{:type :add :argument 3}] (optimizer/optimize-ast-node [{:type :add :argument 2}
+                                                                  {:type :inc}]))))
+
+(deftest combines-substraction-with-dec-test
+  (is (= [{:type :sub :argument 3}] (optimizer/optimize-ast-node [{:type :sub :argument 2}
+                                                                  {:type :dec}]))))
+
+(deftest combines-pointer-addition-with-pointer-inc-test
+  (is (= [{:type :add-pointer :argument 3}] (optimizer/optimize-ast-node [{:type :add-pointer :argument 2}
+                                                                          {:type :inc-pointer}]))))
+
+(deftest combines-substraction-with-dec-test
+  (is (= [{:type :sub-pointer :argument 3}] (optimizer/optimize-ast-node [{:type :sub-pointer :argument 2}
+                                                                          {:type :dec-pointer}]))))
+
+(deftest combines-multiple-dec-into-substracion-test
+  (is (= [{:type :sub :argument 5}] (optimizer/optimize-ast-node [{:type :dec}
+                                                                   {:type :dec}
+                                                                   {:type :dec}
+                                                                   {:type :dec}
+                                                                   {:type :dec}]))))
+
+(deftest combines-multiple-inc-into-addition-test
+  (is (= [{:type :add :argument 4}] (optimizer/optimize-ast-node [{:type :inc}
+                                                                  {:type :inc}
+                                                                  {:type :inc}
+                                                                  {:type :inc}]))))
+
+(deftest combines-multiple-pointer-inc-into-pointer-addition-test
+  (is (= [{:type :add-pointer :argument 4}] (optimizer/optimize-ast-node [{:type :inc-pointer}
+                                                                          {:type :inc-pointer}
+                                                                          {:type :inc-pointer}
+                                                                          {:type :inc-pointer}]))))
+
+(deftest combines-multiple-pointer-dec-into-pointer-substraction-test
+  (is (= [{:type :sub-pointer :argument 4}] (optimizer/optimize-ast-node [{:type :dec-pointer}
+                                                                          {:type :dec-pointer}
+                                                                          {:type :dec-pointer}
+                                                                          {:type :dec-pointer}]))))
+
+(deftest dose-not-combine-if-cannot-test
+  (let [unoptimizable-code [{:type :inc}
+                            {:type :dec}
+                            {:type :inc}
+                            {:type :dec}]]
+    (is (= unoptimizable-code (optimizer/optimize-ast-node unoptimizable-code)))))
+
+;; That is a bad name
+(deftest handles-complicated-sentences-test
+  (let [sentence           [{:type :dec}
+                            {:type :dec}
+                            {:type :dec}
+                            {:type :inc}
+                            {:type :inc}
+                            {:type :inc}
+                            {:type :dec}
+                            {:type :inc}]
+        optimized-sentence [{:type :sub :argument 3}
+                            {:type :add :argument 3}
+                            {:type :inc}
+                            {:type :dec}]]
+    (is (= optimized-sentence (optimizer/optimize-ast-node sentence)))))
