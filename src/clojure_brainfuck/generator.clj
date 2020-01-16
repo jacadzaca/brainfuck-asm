8435f36b28d6d0f24fa6b30e33aa66dbb9c86b3e
jacadzaca <vitouejj@gmail.com>
2020-01-16 19:26:02 +0100

    defined logic for turning a statement into asm

--- src/clojure_brainfuck/generator.clj
+++ src/clojure_brainfuck/generator.clj
@@ -14,6 +14,21 @@
   (generate-label "print_character" "push eax" "push ecx" "push ebx" "push edx" "mov ecx, eax"
                                      "mov eax, 0x04" "mov ebx, 0x01" "mov edx, 0x01" "int 0x80" "pop edx" "pop ebx" "pop ecx" "pop eax" "ret"))
 
+(defn- statement->asm [statement]
+  "Translates an statement to assembly code"
+  (case (:type statement)
+    :inc         "inc byte [eax]"
+    :dec         "dec byte [eax]"
+    :inc-pointer "inc eax"
+    :dec-pointer "dec eax"
+    :add         (str "add byte [eax], " (:argument statement))
+    :sub         (str "sub byte [eax], " (:argument statement))
+    :add-pointer (str "add eax, " (:argument statement))
+    :sub-pointer (str "sub eax, " (:argument statement))
+    :call-print  "call print"
+    :call-read   "call read"
+    :call-loop   (str "call loop" (:argument statement))))
+
 (defn generate-assembly [ast]
   (dotimes [i (count ast)]
     (cond
