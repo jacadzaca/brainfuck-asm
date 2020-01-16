519c02fdc5f2e30e836016bb9e0d2ab3c1233e8c
jacadzaca <vitouejj@gmail.com>
2020-01-16 19:31:46 +0100

    generator is now accesible from the repl

--- src/clojure_brainfuck/core.clj
+++ src/clojure_brainfuck/core.clj
@@ -1,6 +1,7 @@
 (ns clojure-brainfuck.core
   (:require [clojure-brainfuck.parser :as parser]
-            [clojure-brainfuck.optimizer :as optimizer])
+            [clojure-brainfuck.optimizer :as optimizer]
+            [clojure-brainfuck.generator :as generator])
   (:gen-class))
 
 (defn -main
