92d2fa4de80837c14676f9e48c3b21c15cbcca28
jacadzaca <vitouejj@gmail.com>
2020-01-16 18:13:31 +0100

    clarified test cases

--- test/clojure_brainfuck/optimizer_test.clj
+++ test/clojure_brainfuck/optimizer_test.clj
@@ -2,64 +2,48 @@
   (:require [clojure.test :refer [deftest is]]
             [clojure-brainfuck.optimizer :as optimizer]))
 
-(deftest combines-inc-into-addition-test
+(deftest combines-two-inc-instructions-into-addition-test
   (is (= [{:type :add :argument 2}] (optimizer/optimize-sentence [{:type :inc}
                                                                   {:type :inc}]))))
 
-(deftest combines-inc-into-addition-test
+(deftest combines-two-dec-instructions-into-substraction-test
   (is (= [{:type :sub :argument 2}] (optimizer/optimize-sentence [{:type :dec}
                                                                   {:type :dec}]))))
 
-(deftest combines-inc-pointer-into-pointer-addition-test
+(deftest combines-two-inc-pointer-instructions-into-pointer-addition-test
   (is (= [{:type :add-pointer :argument 2}] (optimizer/optimize-sentence [{:type :inc-pointer}
                                                                            {:type :inc-pointer}]))))
 
-(deftest combines-dec-pointer-into-pointer-substracion-test
+(deftest combines-two-dec-pointer-instructions-into-pointer-substracion-test
   (is (= [{:type :sub-pointer :argument 2}] (optimizer/optimize-sentence [{:type :dec-pointer}
                                                                           {:type :dec-pointer}]))))
 
-(deftest combines-addition-with-inc-test
-  (is (= [{:type :add :argument 3}] (optimizer/optimize-sentence [{:type :add :argument 2}
-                                                                  {:type :inc}]))))
-
-(deftest combines-substraction-with-dec-test
-  (is (= [{:type :sub :argument 3}] (optimizer/optimize-sentence [{:type :sub :argument 2}
-                                                                  {:type :dec}]))))
-
-(deftest combines-pointer-addition-with-pointer-inc-test
-  (is (= [{:type :add-pointer :argument 3}] (optimizer/optimize-sentence [{:type :add-pointer :argument 2}
-                                                                          {:type :inc-pointer}]))))
-
-(deftest combines-substraction-with-dec-test
-  (is (= [{:type :sub-pointer :argument 3}] (optimizer/optimize-sentence [{:type :sub-pointer :argument 2}
-                                                                          {:type :dec-pointer}]))))
-
-(deftest combines-multiple-dec-into-substracion-test
+(deftest combines-multiple-dec-instructions-into-substracion-test
   (is (= [{:type :sub :argument 5}] (optimizer/optimize-sentence [{:type :dec}
                                                                    {:type :dec}
                                                                    {:type :dec}
                                                                    {:type :dec}
                                                                    {:type :dec}]))))
 
-(deftest combines-multiple-inc-into-addition-test
+(deftest combines-multiple-inc-instructions-into-addition-test
   (is (= [{:type :add :argument 4}] (optimizer/optimize-sentence [{:type :inc}
                                                                   {:type :inc}
                                                                   {:type :inc}
                                                                   {:type :inc}]))))
 
-(deftest combines-multiple-pointer-inc-into-pointer-addition-test
+(deftest combines-multiple-pointer-inc-instructions-into-pointer-addition-test
   (is (= [{:type :add-pointer :argument 4}] (optimizer/optimize-sentence [{:type :inc-pointer}
                                                                           {:type :inc-pointer}
                                                                           {:type :inc-pointer}
                                                                           {:type :inc-pointer}]))))
 
-(deftest combines-multiple-pointer-dec-into-pointer-substraction-test
+(deftest combines-multiple-pointer-dec-instructions-into-pointer-substraction-test
   (is (= [{:type :sub-pointer :argument 4}] (optimizer/optimize-sentence [{:type :dec-pointer}
                                                                           {:type :dec-pointer}
                                                                           {:type :dec-pointer}
                                                                           {:type :dec-pointer}]))))
 
-(deftest dose-not-combine-if-cannot-test
+(deftest changes-nothing-if-cannot-optimize-test
   (let [unoptimizable-code [{:type :inc}
                             {:type :dec}
                             {:type :inc}
