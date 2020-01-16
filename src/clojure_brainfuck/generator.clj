05234fdfae619b03275b32086c78530fae8febe7
jacadzaca <vitouejj@gmail.com>
2020-01-16 19:31:26 +0100

    generate-assembly now turns an ast into asb

--- src/clojure_brainfuck/generator.clj
+++ src/clojure_brainfuck/generator.clj
@@ -14,6 +14,8 @@
   (generate-label "print_character" "push eax" "push ecx" "push ebx" "push edx" "mov ecx, eax"
                                      "mov eax, 0x04" "mov ebx, 0x01" "mov edx, 0x01" "int 0x80" "pop edx" "pop ebx" "pop ecx" "pop eax" "ret"))
 
+(def :private exit (generate-label "exit" "mov eax, 1" "xor ebx, ebx" "int 0x80"))
+
 (defn- statement->asm [statement]
   "Translates an statement to assembly code"
   (case (:type statement)
@@ -30,7 +32,9 @@
     :call-loop   (str "call loop" (:argument statement))))
 
 (defn generate-assembly [ast]
-  (dotimes [i (count ast)]
-    (cond
-      (str/includes? (nth (keys ast) i) "loop") (println (generate-loop (nth (keys ast) i) (nth (vals ast) i)))
-      :else (println (generate-label (nth (keys ast) i) (nth (vals ast) i))))))
+  (apply str
+    (generate-segment "bss" "array: resb 30000")
+    \newline
+    (generate-segment "text" "global _start")
+    (map #(apply generate-label (:name % "_start") (map statement->asm (:statements %))) ast)
+    exit))
