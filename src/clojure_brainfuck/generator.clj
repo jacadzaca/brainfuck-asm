6b7833e88960eaab3fc2150e372b2aa1225c871e
jacadzaca <vitouejj@gmail.com>
2020-01-17 19:03:06 +0100

    generated asm code is now more readable

--- src/clojure_brainfuck/generator.clj
+++ src/clojure_brainfuck/generator.clj
@@ -5,34 +5,34 @@
   (str "segment ." name \newline (str/join \newline statements) \newline))
 
 (defn- generate-label [name & statements]
-  (str name \: \newline (str/join \newline (flatten statements)) \newline))
+  (str \newline name \: \newline (str/join \newline (flatten statements)) \newline))
 
 (def ^:private print-character
   (generate-label "print_character" "push eax" "push ecx" "push ebx" "push edx" "mov ecx, eax"
                                      "mov eax, 0x04" "mov ebx, 0x01" "mov edx, 0x01" "int 0x80" "pop edx" "pop ebx" "pop ecx" "pop eax" "ret"))
 
-(def ^:private exit (generate-label "exit" "mov eax, 1" "xor ebx, ebx" "int 0x80"))
+(def ^:private exit (generate-label "exit" "    mov eax, 1" "    xor ebx, ebx" "    int 0x80"))
 
 (defn- statement->asm [statement]
   "Translates an statement to assembly code"
   (case (:type statement)
-    :inc         "inc byte [eax]"
-    :dec         "dec byte [eax]"
-    :inc-pointer "inc eax"
-    :dec-pointer "dec eax"
-    :add         (str "add byte [eax], " (:argument statement))
-    :sub         (str "sub byte [eax], " (:argument statement))
-    :add-pointer (str "add eax, " (:argument statement))
-    :sub-pointer (str "sub eax, " (:argument statement))
-    :call-print  "call print"
-    :call-read   "call read"
-    :call-loop   (str "call loop" (:argument statement))
+    :inc         "    inc byte [eax]"
+    :dec         "    dec byte [eax]"
+    :inc-pointer "    inc eax"
+    :dec-pointer "    dec eax"
+    :add         (str "    add byte [eax], " (:argument statement))
+    :sub         (str "    sub byte [eax], " (:argument statement))
+    :add-pointer (str "    add eax, " (:argument statement))
+    :sub-pointer (str "    sub eax, " (:argument statement))
+    :call-print  "    call print"
+    :call-read   "    call read"
+    :call-loop   (str "    call loop" (:argument statement))
     (throw (IllegalArgumentException. (str (:type statement) " is not a proper statement type")))))
 
 (defn generate-assembly [ast]
   (str
-    (generate-segment "bss" "array: resb 30000")
+    (generate-segment "bss" "    array: resb 30000")
     \newline
-    (generate-segment "text" "global _start")
+    (generate-segment "text" "    global _start")
     (apply str (map #(apply generate-label (:name % "_start") (map statement->asm (:statements %))) ast))
     exit))
--- test/clojure_brainfuck/generator_test.clj
+++ test/clojure_brainfuck/generator_test.clj
@@ -23,37 +23,37 @@
     (is (re-find #"segment .text\n\s*\w*\s*global _start\n" generated-asm))))
 
 (deftest inc-statement-properly-translated
-  (is (= "inc byte [eax]" (@#'generator/statement->asm {:type :inc}))))
+  (is (re-matches #"( |\t)*inc byte \[eax\]" (@#'generator/statement->asm {:type :inc}))))
 
 (deftest dec-statement-properly-translated
-  (is (= "dec byte [eax]" (@#'generator/statement->asm {:type :dec}))))
+  (is (re-matches #"( |\t)*dec byte \[eax\]" (@#'generator/statement->asm {:type :dec}))))
 
 (deftest inc-pointer-statement-properly-translated
-  (is (= "inc eax" (@#'generator/statement->asm {:type :inc-pointer}))))
+  (is (re-matches #"( |\t)*inc eax" (@#'generator/statement->asm {:type :inc-pointer}))))
 
 (deftest dec-pointer-statement-properly-translated
-  (is (= "dec eax" (@#'generator/statement->asm {:type :dec-pointer}))))
+  (is (re-matches #"( |\t)*dec eax" (@#'generator/statement->asm {:type :dec-pointer}))))
 
 (deftest add-statement-properly-translated
-  (is (= "add byte [eax], 2" (@#'generator/statement->asm {:type :add :argument 2}))))
+  (is (re-matches #"( |\t)*add byte \[eax\], 2" (@#'generator/statement->asm {:type :add :argument 2}))))
 
 (deftest sub-statement-properly-translated
-  (is (= "sub byte [eax], 2" (@#'generator/statement->asm {:type :sub :argument 2}))))
+  (is (re-matches #"( |\t)*sub byte \[eax\], 2" (@#'generator/statement->asm {:type :sub :argument 2}))))
 
 (deftest add-pointer-statement-properly-translated
-  (is (= "add eax, 2" (@#'generator/statement->asm {:type :add-pointer :argument 2}))))
+  (is (re-matches #"( |\t)*add eax, 2" (@#'generator/statement->asm {:type :add-pointer :argument 2}))))
 
 (deftest sub-pointer-statement-properly-translated
-  (is (= "sub eax, 2" (@#'generator/statement->asm {:type :sub-pointer :argument 2}))))
+  (is (re-matches #"( |\t)*sub eax, 2" (@#'generator/statement->asm {:type :sub-pointer :argument 2}))))
 
 (deftest call-print-statement-properly-translated
-  (is (= "call print" (@#'generator/statement->asm {:type :call-print}))))
+  (is (re-matches #"( |\t)*call print" (@#'generator/statement->asm {:type :call-print}))))
 
 (deftest call-read-statement-properly-translated
-  (is (= "call read" (@#'generator/statement->asm {:type :call-read}))))
+  (is (re-matches #"( |\t)*call read" (@#'generator/statement->asm {:type :call-read}))))
 
 (deftest call-loop-statement-properly-translated
-  (is (= "call loop1" (@#'generator/statement->asm {:type :call-loop :argument 1}))))
+  (is (re-matches #"( |\t)*call loop1" (@#'generator/statement->asm {:type :call-loop :argument 1}))))
 
 (deftest statement->asm-throws-illegal-argument-when-cannot-translate-statement
   (is (thrown-with-msg? IllegalArgumentException #":invalid is not a proper statement type" (@#'generator/statement->asm {:type :invalid}))))
