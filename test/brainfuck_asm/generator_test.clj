5c0a6dfa24bb80bc576be37752e88ece1d3a1993
jacadzaca <vitouejj@gmail.com>
2020-03-19 00:33:21 +0100

    refactored generator test to use a macro

--- test/brainfuck_asm/generator_test.clj
+++ test/brainfuck_asm/generator_test.clj
@@ -22,44 +22,28 @@
   (deftest is-global-start-defined-in-text-segment-test
     (is (re-find #"segment .text\n\s*\w*\s*global _start\n" generated-asm))))
 
-(deftest inc-statement-properly-translated
-  (is (= "inc byte [eax]" (@#'generator/statement->asm {:type :inc}))))
+(defmacro test-statement-properly-translated
+  [statement-name translation to-translate]
+  `((deftest ~(symbol (str statement-name "-statement-properly-translated"))
+         (is (= ~translation (@#'generator/statement->asm  ~to-translate))))))
 
-(deftest dec-statement-properly-translated
-  (is (= "dec byte [eax]" (@#'generator/statement->asm {:type :dec}))))
+(test-statement-properly-translated inc         "inc byte [eax]"    {:type :inc})
+(test-statement-properly-translated dec         "dec byte [eax]"    {:type :dec})
+(test-statement-properly-translated inc-pointer "inc eax"           {:type :inc-pointer})
+(test-statement-properly-translated dec-pointer "dec eax"           {:type :dec-pointer})
+(test-statement-properly-translated add         "add byte [eax], 2" {:type :add :argument 2})
+(test-statement-properly-translated sub         "sub byte [eax], 2" {:type :sub :argument 2})
+(test-statement-properly-translated add-pointer "add eax, 2"        {:type :add-pointer :argument 2})
+(test-statement-properly-translated sub-pointer "sub eax, 2"        {:type :sub-pointer :argument 2})
+(test-statement-properly-translated call-print  "call print_cell"   {:type :call-print})
+(test-statement-properly-translated call-read   "call read"         {:type :call-read})
+(test-statement-properly-translated call-loop   "call loop1"        {:type :call-loop :argument 1})
+(test-statement-properly-translated load-array  "mov eax, array"    {:type :load-array})
 
-(deftest inc-pointer-statement-properly-translated
-  (is (= "inc eax" (@#'generator/statement->asm {:type :inc-pointer}))))
-
-(deftest dec-pointer-statement-properly-translated
-  (is (= "dec eax" (@#'generator/statement->asm {:type :dec-pointer}))))
-
-(deftest add-statement-properly-translated
-  (is (= "add byte [eax], 2" (@#'generator/statement->asm {:type :add :argument 2}))))
-
-(deftest sub-statement-properly-translated
-  (is (= "sub byte [eax], 2" (@#'generator/statement->asm {:type :sub :argument 2}))))
-
-(deftest add-pointer-statement-properly-translated
-  (is (= "add eax, 2" (@#'generator/statement->asm {:type :add-pointer :argument 2}))))
-
-(deftest sub-pointer-statement-properly-translated
-  (is (= "sub eax, 2" (@#'generator/statement->asm {:type :sub-pointer :argument 2}))))
-
-(deftest call-print-statement-properly-translated
-  (is (= "call print_cell" (@#'generator/statement->asm {:type :call-print}))))
-
-(deftest call-read-statement-properly-translated
-  (is (= "call read" (@#'generator/statement->asm {:type :call-read}))))
-
-(deftest call-loop-statement-properly-translated
-  (is (= "call loop1" (@#'generator/statement->asm {:type :call-loop :argument 1}))))
-
-(deftest call-exit-statement-properly-translated
-  (is (= ["mov eax, 1" "xor ebx, ebx" "int 0x80"] (@#'generator/statement->asm {:type :call-exit}))))
-
-(deftest loop-end-statement-properly-translated
-  (is (= ["cmp byte [eax], 0" (str "jne " "loop1") "ret"] (@#'generator/statement->asm {:type :loop-end :argument "loop1"}))))
+(let [call-exit-translation ["mov eax, 1" "xor ebx, ebx" "int 0x80"]
+      loop-end-translation  ["cmp byte [eax], 0" "jne loop1" "ret"]]
+      (test-statement-properly-translated call-exit call-exit-translation {:type :call-exit})
+      (test-statement-properly-translated loop-end loop-end-translation   {:type :loop-end :argument "loop1"}))
 
 (deftest statement->asm-throws-illegal-argument-when-cannot-translate-statement
   (is (thrown-with-msg? IllegalArgumentException #":invalid is not a proper statement type" (@#'generator/statement->asm {:type :invalid}))))
