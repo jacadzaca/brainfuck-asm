06dbcc85b6dab20140c6e14c09a0c845c6b327a3
jacadzaca <vitouejj@gmail.com>
2020-03-19 16:27:06 +0100

    program dosent crush, when supplied with wrong input

--- src/brainfuck_asm/core.clj
+++ src/brainfuck_asm/core.clj
@@ -1,7 +1,8 @@
 (ns brainfuck-asm.core
   (:require [brainfuck-asm.parser :as parser]
             [brainfuck-asm.optimizer :as optimizer]
-            [brainfuck-asm.generator :as generator])
+            [brainfuck-asm.generator :as generator]
+            [clojure.java.io :as io])
   (:gen-class))
 
 (defn- optimize-ast [ast]
@@ -11,5 +12,17 @@
   (let [legal-characters #{\+ \- \< \> \. \, \[ \]}]
     (filter #(contains? legal-characters %) input)))
 
-(defn -main [input-file-name]
-  (-> input-file-name slurp parser/remove-initial-comment-loop sanitize-input parser/generate-ast optimize-ast generator/generate-assembly print))
+(defn -main [& args]
+  (let [input-file-name (first args)]
+    (cond 
+      (= input-file-name nil) (println "Please specify a brainfuck source file to compile")
+      (.exists (io/file input-file-name))
+        (-> (first args) 
+            slurp 
+            parser/remove-initial-comment-loop
+            sanitize-input 
+            parser/generate-ast 
+            optimize-ast 
+            generator/generate-assembly 
+            println)
+      :else (println (str "Cannot find: " input-file-name "\nExiting...")))))
