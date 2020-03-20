627f929b19701184d5a5d4dc4eaec947554a729d
jacadzaca <vitouejj@gmail.com>
2020-03-20 15:10:20 +0100

    quoted symbols

--- test/brainfuck_asm/generate_test.clj
+++ test/brainfuck_asm/generate_test.clj
@@ -25,20 +25,20 @@
 (defmacro test-statement-properly-translated
   [statement-name translation to-translate]
   `((deftest ~(symbol (str statement-name "-statement-properly-translated"))
-      (is (= ~translation (@#'generate/statement->asm  ~to-translate))))))
+      (is (= ~translation (`@#'generate/statement->asm  ~to-translate))))))
 
-(test-statement-properly-translated inc         "inc byte [eax]"    {:type :inc})
-(test-statement-properly-translated dec         "dec byte [eax]"    {:type :dec})
-(test-statement-properly-translated inc-pointer "inc eax"           {:type :inc-pointer})
-(test-statement-properly-translated dec-pointer "dec eax"           {:type :dec-pointer})
-(test-statement-properly-translated add         "add byte [eax], 2" {:type :add :argument 2})
-(test-statement-properly-translated sub         "sub byte [eax], 2" {:type :sub :argument 2})
-(test-statement-properly-translated add-pointer "add eax, 2"        {:type :add-pointer :argument 2})
-(test-statement-properly-translated sub-pointer "sub eax, 2"        {:type :sub-pointer :argument 2})
-(test-statement-properly-translated call-print  "call print_cell"   {:type :call-print})
-(test-statement-properly-translated call-read   "call read"         {:type :call-read})
-(test-statement-properly-translated call-loop   "call loop1"        {:type :call-loop :argument 1})
-(test-statement-properly-translated load-array  "mov eax, array"    {:type :load-array})
+(test-statement-properly-translated `inc         "inc byte [eax]"    {:type :inc})
+(test-statement-properly-translated `dec         "dec byte [eax]"    {:type :dec})
+(test-statement-properly-translated `inc-pointer "inc eax"           {:type :inc-pointer})
+(test-statement-properly-translated `dec-pointer "dec eax"           {:type :dec-pointer})
+(test-statement-properly-translated `add         "add byte [eax], 2" {:type :add :argument 2})
+(test-statement-properly-translated `sub         "sub byte [eax], 2" {:type :sub :argument 2})
+(test-statement-properly-translated `add-pointer "add eax, 2"        {:type :add-pointer :argument 2})
+(test-statement-properly-translated `sub-pointer "sub eax, 2"        {:type :sub-pointer :argument 2})
+(test-statement-properly-translated `call-print  "call print_cell"   {:type :call-print})
+(test-statement-properly-translated `call-read   "call read"         {:type :call-read})
+(test-statement-properly-translated `call-loop   "call loop1"        {:type :call-loop :argument 1})
+(test-statement-properly-translated `load-array  "mov eax, array"    {:type :load-array})
 
 (let [call-exit-translation ["mov eax, 1" "xor ebx, ebx" "int 0x80"]
       loop-end-translation  ["cmp byte [eax], 0" "jne loop1" "ret"]]
